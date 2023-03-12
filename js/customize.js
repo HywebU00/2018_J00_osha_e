8e3a5e46cd388527448d6a166693df88d67426e9
anniewu <annie@hyweb.com.tw>
2023-03-13 00:01:29 +0800

    20230312新增跑馬燈

--- js/customize.js
+++ js/customize.js
@@ -336,3 +336,22 @@ $(function(){
         $('.condition_searchblock').stop().slideToggle();
     })
 })
+// 跑馬燈
+$(function() {
+    // 跑馬燈
+    if ($('.marquee').length > 0) {
+        $('.marquee ul').slick({
+            dots: false,
+            infinite: true,
+            vertical: true,
+            verticalSwiping: true,
+            speed: 300,
+            autoplaySpeed: 5000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            pauseOnHover: true, //滑鼠移過後暫停自動撥放
+            focusOnSelect: true,
+        });
+    }
+})
--- js/hyui.js
+++ js/hyui.js
@@ -168,11 +168,11 @@ $(function() {
             // 副選單滑出
             liHasChild.on({
                 mouseenter: function() {
-                    $(this).children('ul').stop(true, false).fadeIn();
+                    $(this).children('ul').stop(true, false).fadeIn(200);
                 },
                 mouseleave: function() {
                     $(this).parent().siblings('ul').hide();
-                    $(this).children('ul').stop(true, false).fadeOut();
+                    $(this).children('ul').stop(true, false).fadeOut(200);
                 }
             });
             liHasChild.off('click');
